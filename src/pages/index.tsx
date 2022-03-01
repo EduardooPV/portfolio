a03858661aa6d60508b5369ed1b5bd34e2817c71
EduuardoPV <eduardoveltroni@hotmail.com>
2022-03-01 20:10:57 -0300

    Feat: index.js rendering with components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { GlobalStyle } from "../styles/GlobalStyle";
-
-import { ThemeProvider } from "styled-components";
 import { combineTheme, dark, light } from "../styles/theme";
-import usePersistedState from '../utils/usePersistedState'
-import { WidthProvider } from "../utils/useWidth";
 
+import { GlobalStyle } from "../styles/GlobalStyle";
+import { Hero } from "../components/Hero";
 import { Navbar } from "../components/Navbar";
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { WidthProvider } from "../utils/useWidth";
+import usePersistedState from '../utils/usePersistedState'
 
 export default function Home() {
   const [theme, setTheme] = usePersistedState('theme', combineTheme(dark));
@@ -20,6 +20,7 @@ export default function Home() {
       <WidthProvider>
         <GlobalStyle />
         <Navbar toggleTheme={toggleTheme} theme={theme}/>
+        <Hero />
       </WidthProvider>
     </ThemeProvider>
   )
