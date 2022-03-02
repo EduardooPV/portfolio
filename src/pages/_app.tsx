8b90494bdeb3cb9ed22cd36ad0c5f6458efc7b03
EduuardoPV <eduardoveltroni@hotmail.com>
2022-03-02 08:56:19 -0300

    Refactor: corrects global properties

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,33 @@
 import React from "react";
 import { AppProps } from "next/app";
 
+import { ThemeProvider } from "styled-components";
+import { WidthProvider } from "../utils/useWidth";
+import { GlobalStyle } from "../styles/GlobalStyle";
+import { Navbar } from "../components/Navbar";
+import { combineTheme, dark, light } from "../styles/theme";
+import usePersistedState from "../utils/usePersistedState";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [theme, setTheme] = usePersistedState("theme", combineTheme(dark));
+
+  const toggleTheme = () => {
+    setTheme(
+      theme.title === "light" ? combineTheme(dark) : combineTheme(light)
+    );
+  };
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <WidthProvider>
+          <GlobalStyle />
+          <Navbar toggleTheme={toggleTheme} theme={theme} />
+          <Component {...pageProps} />
+        </WidthProvider>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,12 @@
-import { combineTheme, dark, light } from "../styles/theme";
-
-import { GlobalStyle } from "../styles/GlobalStyle";
-import { Hero } from "../components/Hero";
-import { Navbar } from "../components/Navbar";
 import React from "react";
-import { ThemeProvider } from "styled-components";
-import { WidthProvider } from "../utils/useWidth";
-import usePersistedState from '../utils/usePersistedState'
 import { Diviser } from "../components/Diviser";
+import { Hero } from "../components/Hero";
 
 export default function Home() {
-  const [theme, setTheme] = usePersistedState('theme', combineTheme(dark));
-
-  const toggleTheme = () => {
-    setTheme(theme.title === 'light' ? combineTheme(dark) : combineTheme(light));
-  };
-
-  return  (
-    <ThemeProvider theme={theme}>
-      <WidthProvider>
-        <GlobalStyle />
-        <Navbar toggleTheme={toggleTheme} theme={theme}/>
-        <Hero />
-
-        <Diviser /> 
-
-      </WidthProvider>
-    </ThemeProvider>
-  )
+  return (
+    <>
+      <Hero />
+      <Diviser />
+    </>
+  );
 }
