7d393fe738f78d15c70f5ae082e9509d93019ef5
EduardooPV <eduardoveltroni@hotmail.com>
2022-03-15 15:53:44 -0300

    Feat: add index tp vercel view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Diviser } from "../components/Diviser";
 import { Hero } from "../components/Hero";
+import { About } from "../components/About";
+import { Skills } from "../components/Skills";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Diviser />
+      <About />
+      <Diviser />
+      <Skills />
     </>
   );
 }
