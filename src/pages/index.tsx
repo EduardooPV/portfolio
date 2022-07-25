9d646890280218a029ba7d55c2a6d6ec1727acaa
EduardooPV <eduardoveltroni@hotmail.com>
2022-07-25 09:24:28 -0300

    chore: add hotjar lib

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 import { Diviser } from "../components/Diviser";
 import { Hero } from "../components/Hero";
@@ -8,7 +8,17 @@ import { Projects } from "../components/Projects";
 import { Footer } from "../components/Footer";
 import { BackToHome } from "../components/BackToHome";
 
+import { hotjar } from "react-hotjar";
+
 export default function Home() {
+
+  const hjid = 3080565;
+  const hjsv = 6
+
+  useEffect(() => {
+    hotjar.initialize(hjid, hjsv);
+  });
+
   return (
     <>
       <Head>
