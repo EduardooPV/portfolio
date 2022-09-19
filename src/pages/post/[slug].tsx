7e99b604cc80bc5a4071fa71c47bd3098e11a8a1
EduardooPV <eduardoveltroni@hotmail.com>
2022-09-19 18:06:26 -0300

    feat: create dynamic page of post

---
+++ src/pages/post/[slug].tsx
@@ -0,0 +1,163 @@
+import React from "react";
+import Head from "next/head";
+import { GetStaticProps } from "next";
+import { useMemo } from "react";
+import { useRouter } from "next/router";
+
+import { RichText } from "prismic-dom";
+import * as prismic from "@prismicio/client";
+import { client } from "../../services/prismic";
+
+import { CalendarBlank, Clock, Tag } from "phosphor-react";
+
+import { ParsedUrlQuery } from "querystring";
+
+import { AsidePerson } from "../../components/AsidePerson";
+import { ReadingProgressiveBar } from "../../components/ReadingProgressiveBar";
+
+import { Container, Infos, ContentPost } from "./styles";
+
+interface Post {
+  updatedAt: string;
+
+  data: {
+    title: string;
+    banner: {
+      url?: string;
+      alt?: string;
+    };
+    tag: string;
+    content: {
+      body: {
+        text: string;
+      }[];
+    }[];
+  };
+}
+
+interface PostProps {
+  post: Post;
+}
+
+interface IParams extends ParsedUrlQuery {
+  slug: string;
+}
+
+const Home = ({ post }: PostProps) => {
+  const router = useRouter();
+
+  const readTime = useMemo(() => {
+    if (router.isFallback) {
+      return 0;
+    }
+
+    let fullText = "";
+
+    const readWordsPerMinute = 200;
+
+    post.data.content.forEach((postContent) => {
+      fullText += RichText.asText(postContent.body);
+    });
+
+    const time = Math.ceil(fullText.split(/\s/g).length / readWordsPerMinute);
+
+    return time;
+  }, [post, router.isFallback]);
+
+  return (
+    <>
+      <Head>
+        <title>{post.data.title}</title>
+      </Head>
+
+      <ReadingProgressiveBar />
+
+      <Container>
+        <article>
+          <h1>{post.data.title}</h1>
+
+          <Infos>
+            <li>
+              <CalendarBlank size={20} color="#F89D24" />
+              <span>{post.updatedAt}</span>
+            </li>
+            <li>
+              <Clock size={20} color="#F89D24" />
+              <span>{readTime} min</span>
+            </li>
+            <li>
+              <Tag size={20} color="#F89D24" />
+              {post.data.tag}
+            </li>
+          </Infos>
+
+          <img src={post.data.banner?.url} alt={post.data.banner?.alt} />
+
+          <ContentPost>
+            {post.data.content.map((postContent: any, index) => (
+              <div key={index}>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: RichText.asHtml(postContent.body),
+                  }}
+                />
+              </div>
+            ))}
+
+            <AsidePerson />
+          </ContentPost>
+        </article>
+      </Container>
+    </>
+  );
+};
+
+export default Home;
+
+export const getStaticPaths = async () => {
+  const response = await client.get({
+    predicates: prismic.predicate.at("document.type", "posts"),
+  });
+
+  const paths = response.results.map((post) => ({
+    params: { slug: post.uid },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { slug } = context.params as IParams;
+
+  const response = await client.getByUID("posts", String(slug), {});
+
+  const post = {
+    slug,
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString(
+      "pt-br",
+      {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+      }
+    ),
+    data: {
+      title: response.data.title,
+      banner: {
+        url: response.data.banner.url ? response.data.banner.url : null,
+        alt: response.data.banner.alt ? response.data.banner.alt : null,
+      },
+      tag: response.data.tag,
+      content: response.data.content,
+    },
+  };
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
---
+++ src/pages/post/styles.ts
@@ -0,0 +1,127 @@
+import styled from "styled-components";
+import c from "../../styles/pallete.json";
+
+export const Container = styled.main`
+  max-width: 1120px;
+
+  margin: 0 auto;
+  padding: 40px 20px;
+
+  h1 {
+    margin-top: 50px;
+
+    font-weight: 700;
+    font-size: 35px;
+    line-height: 40px;
+    color: ${c.heading};
+
+    @media (min-width: 768px) {
+      font-size: 48px;
+      line-height: 58px;
+    }
+  }
+
+  span {
+    font-size: 0.875rem;
+    line-height: 1.063rem;
+  }
+
+  img {
+    display: block;
+    width: 100%;
+    max-height: 300px;
+    object-fit: fill;
+    border-radius: 10px;
+  }
+`;
+
+export const Infos = styled.ul`
+  margin: 24px 0 30px 0;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 1.5rem;
+
+  list-style: none;
+
+  li:first-child {
+    text-transform: capitalize;
+  }
+
+  li {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    color: ${c.info};
+  }
+`;
+
+export const ContentPost = styled.section`
+  width: 100%;
+
+  display: grid;
+  grid-template-columns: 1fr;
+  justify-items: center;
+  gap: 20px;
+
+  color: ${c.heading};
+
+  @media (min-width: 768px) {
+    grid-template-columns: 1fr 250px;
+    gap: 20px;
+  }
+
+  @media (min-width: 1024px) {
+    grid-template-columns: 1fr 300px;
+  }
+
+  > div:first-child {
+    margin-top: 1rem;
+    padding: 0 1rem;
+
+    border-radius: 10px;
+    background: #181a1d;
+  }
+
+  h2 {
+    margin: 3rem 0 1rem 0;
+    font-weight: 700;
+    font-size: 28px;
+    line-height: 140%;
+
+    @media (min-width: 768px) {
+      font-size: 36px;
+      line-height: 140%;
+    }
+  }
+
+  p {
+    margin: 1rem 0;
+    font-size: 16px;
+    line-height: 28px;
+    word-wrap: break-word;
+  }
+
+  li {
+    margin: 1rem 0 0 1.1rem;
+    list-style-type: square;
+  }
+
+  pre {
+    background: rgb(63, 63, 63);
+    font-size: 14px;
+    padding: 1rem;
+    border-radius: 4px;
+    white-space: pre-line;
+  }
+
+  a {
+    color: ${c.info};
+    transition: color 0.1s;
+
+    :hover {
+      color: ${c.white};
+    }
+  }
+`;
