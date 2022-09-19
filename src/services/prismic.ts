ce519904e471093b21344d9d899244c5f22128fe
EduardooPV <eduardoveltroni@hotmail.com>
2022-09-19 18:04:50 -0300

    chore: config service to prismicms

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,4 +1,4 @@
 import * as prismic from "@prismicio/client";
 
-const endpoint = prismic.getEndpoint("blogluizeduardo");
+const endpoint = prismic.getEndpoint(process.env.ENDPOINT_PRISMIC);
 export const client = prismic.createClient(endpoint);
