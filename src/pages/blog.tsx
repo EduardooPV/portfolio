e1c38e6d6d93457cb47c317e89f29c2888d968ab
EduardooPV <eduardoveltroni@hotmail.com>
2022-09-22 23:18:22 -0300

    refactor: change date publication, pick first date

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -43,13 +43,15 @@ export const getStaticProps: GetStaticProps = async () => {
     predicates: prismic.predicate.at("document.type", "posts"),
   });
 
+  console.log(response)
+
   const posts = response.results.map((post) => {
     return {
       slug: post.uid,
       title: post.data.title,
       description: post.data.description,
       tag: post.data.tag,
-      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+      updatedAt: new Date(post.first_publication_date).toLocaleDateString(
         "pt-br",
         {
           day: "2-digit",
