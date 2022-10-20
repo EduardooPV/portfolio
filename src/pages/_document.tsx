e182257896e911a4dc8e7b39b01a4ee890a21d5e
EduardooPV <eduardoveltroni@hotmail.com>
2022-10-20 17:18:03 -0300

    fix: connect with google fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -40,12 +40,14 @@ class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preload" as="font" />
+
           <link
-            href="https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,regular,italic,500,500italic,700,700italic,900,900italic"
+            href="https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,regular,italic,500,500italic,700,700italic,900,900italic&display=swap"
             rel="stylesheet"
           />
           <link
-            href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,regular,500,600,700,800,900,100italic,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic"
+            href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,regular,500,600,700,800,900,100italic,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic&display=swap"
             rel="stylesheet"
           />
 
